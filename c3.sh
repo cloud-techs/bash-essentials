a319ed97fe850776ad93dd6d1bfeb5fcb6fe822b
cloud-tech <50176501+cloud-techs@users.noreply.github.com>
2020-10-11 13:06:50 +0530

    null

--- c3.sh
+++ c3.sh
@@ -5,3 +5,20 @@ s1=""
 s2="something"
 [[ -z $s1 && -n $s2 ]]
 echo $?
+
+
+# -n check for not null
+
+[[ -n $s1 ]]
+echo $?
+
+# -z check for null string
+
+[[ -z $s2 ]]
+echo $?
+
+[[ -z $s1 ]]
+echo $?
+
+[[ -n $s2 ]]
+echo $?
