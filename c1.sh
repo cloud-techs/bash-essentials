25575b19cbccc762b80fba8c46bd0cbfc03f7c73
cloud-tech <50176501+cloud-techs@users.noreply.github.com>
2020-10-11 13:18:12 +0530

    number check

--- c1.sh
+++ c1.sh
@@ -2,7 +2,13 @@
 pt=$(ping -c 1 gcptutorials.com | grep "bytes from" | cut -d = -f 4)
 echo "Ping time is $pt"
 
-
+# Check numbers in string
+s1=" Simple string with no numbers"
+if [[ $s1 ~= [0-9]+ ]]; then
+    echo "There is digit in $s1"
+else
+    echo "There ia no digit in $s1"
+fi
 
 
 
