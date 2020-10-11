27228430913360436d53920d06c960cb7a8a6187
cloud-tech <50176501+cloud-techs@users.noreply.github.com>
2020-10-11 13:19:39 +0530

    nc

--- c1.sh
+++ c1.sh
@@ -4,12 +4,17 @@ echo "Ping time is $pt"
 
 # Check numbers in string
 s1=" Simple string with no numbers"
-if [[ $s1 ~= [0-9]+ ]]; then
+if [[ $s1 =~ [0-9]+ ]]; then
     echo "There is digit in $s1"
 else
     echo "There ia no digit in $s1"
 fi
 
 
-
+s2=" Simple string with 123 numbers"
+if [[ $s1 =~ [0-9]+ ]]; then
+    echo "There is digit in $s2"
+else
+    echo "There ia no digit in $s2"
+fi
 
